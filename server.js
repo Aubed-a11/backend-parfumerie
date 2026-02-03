7be1b677055ff2a6f173fea8aedaf597fd7e15f4
Aubed-a11 <horticesedjroaubed@gmail.com>
2026-02-03 14:56:20 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import cors from "cors";
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
@@ -11,7 +12,11 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 mongoose
